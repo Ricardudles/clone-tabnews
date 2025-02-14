bedff4cb4d3784c76693616a2334168fa167bc72
Ricardo Maciel <42289437+Ricardudles@users.noreply.github.com>
2025-02-14 04:22:59 +0000

    feat: Add wait-for-postgres script and update dev script for migration handling

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,31 @@
+const { exec } = require("node:child_process");
+
+let counter = 0;
+let mementoMori = ["👶", "👦", "🧑", "🧓", "👴", "💀"];
+let hourglass = ["⏳", "⌛", "⏳", "⌛", "⏳", "⌛"];
+
+function checkPostgres() {
+  exec("docker exec postgres-dev pg_isready --host localhost", handleReturn);
+
+  function handleReturn(error, stdout, stderr) {
+    if (stdout.search("accepting connections") === -1) {
+      process.stdout.write(
+        `\r${hourglass[counter % hourglass.length]}  ${
+          mementoMori[counter % mementoMori.length]
+        }  Awaiting Postgres warmup...`
+      );
+      counter++;
+
+      setTimeout(checkPostgres, 250);
+      return;
+    }
+
+    console.log(
+      `\r${hourglass[counter % hourglass.length]}  ${
+        mementoMori[counter % mementoMori.length]
+      }  Postgres accepting new connections!`
+    );
+  }
+}
+
+checkPostgres();
